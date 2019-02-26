96c5574b1dbbcdd292f30db91f7f5f5ee667e1cc
Binh Nguyen <binhnguyens@live.ca>
2019-02-25 19:10:27 -0500

    Update initial-connects_pt3.ino
    currently commented out fona
    also added bluetooth audio output and audio output to pin A1

--- initial-connects_pt3/initial-connects_pt3.ino
+++ initial-connects_pt3/initial-connects_pt3.ino
@@ -1,188 +1,137 @@
+/* Capstone project */
 
-
-#include "setup_ble.h"
-#include "GPS_and_GSM_Location.h"
 
 char* sendto;
 char* message;
 int count = 0;
 
-
-bool ack_required = false;
-int t_hold;
-int t_present;
-int gps_counter = 1;
-int alert_level = 0;
-
+#include "SD_card.h"
+#include "setup_ble.h"
+//#include "GPS_and_GSM_Location.h"
+#include "alert.h"
 
 void setup() {
   // put your setup code here, to run once:
+
+  delay (250);
   bluetooth_startup();
-  delay (2500);
-  Fona808_intial();
-  delay (2500);
-  Fona808_connect ();
+  delay(2500);
+  init_SD();
+  /* REMOVE
+    delay (2500);
+    Fona808_intial();
+    delay (2500);
+    Fona808_connect ();
+    delay (500);
+  */
+
 }
 
+
+
 void loop() {
+  int t_hold;
+  int t_present;
+  int gps_counter = 1;
+  int alert_level = 0;
+  char filename[27] = "/sdcard/imperial_8_bit.wav";
 
-    /****** Begin Initial Connections ******/
 
-    ble_phone_connection_status();
-    if (deviceConnected)
-    {
-      Serial.println("Bluetooth Phone connect");
-    }
+  /****** Begin Initial Connections ******/
+  ble_phone_connection_status();
+  if (deviceConnected)
+  {
+    Serial.println("Bluetooth Phone connect");
+    delay (1000);
+  }
 
-    /****** Bluetooth to Speakers connection ******/
-    if (bl_speaker)
-    {
-      Serial.println("Bluetooth speaker connect");
-      delay (1000);
-    }
+  /****** Bluetooth to Speakers connection ******/
+  if (bl_speaker)
+  {
+    Serial.println("Bluetooth speaker connect");
+    delay (1000);
+  }
 
-    /****** GPS and GSM connections ******/
-    
+  /****** GPS and GSM connections ******/
+  /*REMOVE
     if (gsmnetwork)
     {
-      Serial.println("GSM connection confirmed");
-      delay (1000);
+    Serial.println("GSM connection confirmed");
+    delay (1000);
     }
-    
-    delay (500);
-    t_hold = millis (); // Gets current time before entering normal loop
-    Serial.println(t_hold);
+  */
 
-            if(bl_speaker && deviceConnected)
-            {
-                  while (1)
-                  {
+  delay (500);
 
-                    
-                      t_present = millis (); // Gets current time    
-                      
-                      Serial.println("Connection successful - move into while loop");    
-                      delay (1000);
-                      alert (alert_level);
-
-                      // Find the GPS every 20 Seconds
-                      if (t_present >=t_hold+20000)
-                      {
-                        Serial.println("Entering gps loop"); 
-                        gps_location ();
-                        retrieve_lat_lon ();
-                        t_hold = t_present;
-                        Serial.println("Latitude");    
-                        Serial.println(lat);    
-                        Serial.println("Latitude");    
-                        Serial.println(lon); 
-                          
-                      }
-
-                      
-                      
-                      // Bluetooth becomes disconnected
-                      ble_phone_connection_status();
-                      if (!deviceConnected) 
-                      {
-                        Serial.println("Entering Bluetooth disconnection test");   
-                        delay (500);
-                        alert_level =2;
-                      }
+  t_hold = millis ();
 
 
-                      
-                      delay (1);
-                      yield (); 
-                      
-                  } // while loop
-            }// if statement
+  // Gets current time before entering normal loop
+  Serial.println(t_hold);
 
-            
+  if (bl_speaker && deviceConnected)
+  {
+
+    while (1)
+    {
+
+
+      t_present = millis (); // Gets current time
+
+      Serial.println("Connection successful - move into while loop");
+      delay (1000);
+      alert_level = alert (alert_level);
+
+
+
+      /*REMOVE
+        // Find the GPS every 20 Seconds
+        if (t_present >=t_hold+20000)
+        {
+        Serial.println("Entering gps loop");
+        gps_location ();
+        retrieve_lat_lon ();
+        t_hold = t_present;
+        Serial.println("Latitude");
+        Serial.println(lat);
+        Serial.println("Latitude");
+        Serial.println(lon);
+
+        }
+      */
+
+      // Easy Alert
+      if (bl_message) // Testing with input from iPhone
+      {
+        bl_message = false;
+        Serial.println("Entering Bluetooth Speaker disconnection test");
+        delay (500);
+        alert_level = 1;
+        //play_audio (filename);
+      }
+
+
+      // Medium Alert
+      // Bluetooth becomes disconnected
+      /* FROG - REMOVE THIS COMMENT
+        ble_phone_connection_status();
+        if (!deviceConnected)
+        {
+        Serial.println("Entering Bluetooth Phone disconnection test");
+        delay (500);
+        alert_level =2;
+        }
+      */
+
+      delay (1);
+      yield ();
+
+    } // while loop
+  }// if statement
+
+
 }// main fct
 
 
 
-
-
-
-
-
-void alert (int alert_level){
-    if (alert_level == 1) // easy alert -> Baby positioned weird, something that needs to be adjusted
-    {
-      delay  (1000);
-      Serial.println("Entering Easy Alert");   
-      easy_alert();
-      delay (1000);
-    }
-
-    else if (alert_level == 2) // medium alert -> BL disconnected, Baby is not there, etc.
-    {
-      delay  (1000);
-      Serial.println("Entering Medium Alert");  
-      medium_alert();
-      delay  (1000);
-    }
-    
-    else if (alert_level == 3) // hard alert -> No response from parent 
-    {
-      delay  (1000);
-      Serial.println("Entering Hard Alert");  
-      hard_alert();
-      delay  (1000);
-    }
-}
-
-void easy_alert (){
-  Serial.println(F("Entering Easy Alert"));
-  delay (1000);
-  Serial.println("----------------------------------------------------");
-  //bt_app_av_media_proc(uint16_t event, void *param)
-  
-  delay (5000);
-  
-  /***** Include parameters to check condition*****/
-    
-}
-
-
-void medium_alert (){
-  while (1){
-  Serial.println(F("Entering Medium Alert"));
-  delay (1000);
-  Serial.println("----------------------------------------------------");
-  sendto = "6477162554";
-  message = "This is a medium alert - Please reset system";
-  text_message (sendto, message);
-  count +=1;
-  Serial.println(count);
-  delay (10000);
-  
-  if (count == 3)
-    {
-        hard_alert();      
-        
-    }
-
-
-    /***** Include parameters to check condition*****/
-    
-  }// while loop
-
-}
-
-
-void hard_alert (){
-  Serial.println(F("Entering Hard Alert"));
-  delay (1000);
-  Serial.println("----------------------------------------------------");
-  sendto = "6477162554";
-  headphone_jack ();
-  phonecall (sendto);
-  delay (100000);
-
-  
-   
-}
 
